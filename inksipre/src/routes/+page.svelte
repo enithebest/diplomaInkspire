1e5ea95eea198d36023d36cbc16d8c01c8d31f15
Gani Hasmegaj <ganhas20@htl-shkoder.com>
2025-10-24 23:53:56 +0200

    opened folders & fixed redirect mistakes on navigation

--- inksipre/src/routes/+page.svelte
+++ inksipre/src/routes/+page.svelte
@@ -2,9 +2,11 @@
   
   import Content from '$lib/components/content/content.svelte';
   import Hero from '$lib/components/herosection/hero.svelte';
+	import Nav from '$lib/components/Nav.svelte';
   import Product from '$lib/components/products/product.svelte';
 
 </script>
+
 
 
 
